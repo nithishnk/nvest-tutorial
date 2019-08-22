fce01fc93753907b2a82e51caf65920931ebbb75
nithishnk <nithish@nvestbank.com>
2019-08-22 15:39:16 +0530

    reg button and image

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 22, 2019 15:37:25").getTime();
+var countDownDate = new Date("Aug 23, 2019 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
