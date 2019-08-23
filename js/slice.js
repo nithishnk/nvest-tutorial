61ac344160123d54addb7d010e7ae5ef7387c1e5
nithishnk <nithish@nvestbank.com>
2019-08-23 15:50:06 +0530

    times

---
+++ js/slice.js
@@ -0,0 +1,33 @@
+var endDate = new Date("Mar 15, 2019 12:00:00").getTime();
+
+var timer = setInterval(function() {
+
+    let now = newDate().getTime();
+    let t = endDate - now;
+    
+    if (t >= 0) {
+    
+        let days = Math.floor(t / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
+    
+        document.getElementById("timer-days").innerHTML = days +
+        "<span class='label'>DAY(S)</span>";
+    
+        document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) +
+        "<span class='label'>HR(S)</span>";
+    
+        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) +
+        "<span class='label'>MIN(S)</span>";
+    
+        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) +
+        "<span class='label'></span>";
+    
+    } else {
+
+        document.getElementById("timer").innerHTML = "The countdown is over!";
+    
+    }
+    
+}, 1000);
--- js/timer.js
+++ js/timer.js
@@ -1,33 +1,4 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Aug 23, 2019 24:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-   
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-
+// document.getElementById("demotimercount").innerHTML = `${h % 12 - pa}` + " : " + `${nk - n}` + " : " + `${60 - aj}`;
 
 var d = new Date();
 var n = d.getMinutes();
@@ -35,9 +6,10 @@ var h = d.getHours();
 var td = d.getDate();
 var aj = d.getSeconds()
 
+document.getElementById("todaydate").innerHTML = d.getDate();
 function setmintime(){
 let minout
-if(n>=0 && n<15){
+if(n>=00 && n<15){
 document.getElementById("demomin").innerHTML = 15;
 minout = 15;
 }
@@ -67,7 +39,7 @@ if(n>=45 && h!=12){
 document.getElementById("demohr").innerHTML = h % 12 + 1 || h + 1;
 hrout = h % 12 + 1 || h + 1;
 }
-else if(h==12){
+else if(h==12 && n==45){
 document.getElementById("demohr").innerHTML = "1";
 hrout = "1";
 }
@@ -80,18 +52,23 @@ return hrout;
 sethrtime();
 
 var pa = sethrtime();
-var nk = setmintime()
+var nk = setmintime();
 
 function countDownTimer(){
-// setInterval(function(){ 
-document.getElementById("demotimercount").innerHTML = `${h % 12 - pa}` + " : " + `${nk - n}` + " : " + `${60 - aj}`;
 
+// setInterval(function(){ 
+document.getElementById("demotimercount").innerHTML =`${nk - n}` + " : " + `${60 - aj}`;
 var uptocount = `${h % 12 - pa}` + `${nk - n}` + `${60 - aj}`
 
 console.log("hour", h % 12)
 console.log("alt hr", pa )
 console.log("second", 60 - aj)
 console.log("hour",`${h % 12 - pa}` + `${nk - n}` + `${60 - aj}`)
-// });
+// }, 1000);
 }
 countDownTimer();
+
+
+
+
+
